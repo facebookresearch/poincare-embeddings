72c64226f28dd47d065bcfb3efe70058d9f38cce
lematt1991 <lematt1991@gmail.com>
2020-01-29 13:01:56 -0800

    Fixing lint errors

--- examples/acl_2019/setup_data.py
+++ examples/acl_2019/setup_data.py
@@ -10,7 +10,7 @@ import numpy as np
 if not os.path.exists('hearst_ppmi_filtered.csv'):
     # Download Hearst counts data
     with NamedTemporaryFile(suffix='.txt.gz') as tfile:
-        url = 'https://github.com/facebookresearch/hypernymysuite/raw/master/hearst_counts.txt.gz'
+        url = 'https://github.com/facebookresearch/hypernymysuite/raw/master/hearst_counts.txt.gz'  # noqa B950
         check_call(['wget', url, '-O', tfile.name])
         df = pandas.read_csv(tfile.name, header=None, sep='\t')
         df.columns = ['id1', 'id2', 'count']
@@ -20,9 +20,14 @@ if not os.path.exists('hearst_ppmi_filtered.csv'):
     pc = df.rename(columns={'count': 'pc'}).groupby('id2')['pc'].sum()
     df = df.merge(pc.to_frame(), on=['id2'], how='left')
     N = len(np.unique(df[['id1', 'id2']].values.reshape(-1)))
-    df['ppmi'] = np.clip(np.log(N) + np.log(df['count']) - np.log(df['pw']) - np.log(df['pc']), 0, 1e12)
+    df['ppmi'] = np.clip(
+        np.log(N) + np.log(df['count']) - np.log(df['pw']) - np.log(df['pc']),
+        0,
+        1e12
+    )
     df = df.rename(columns={'ppmi': 'weight'})
-    df[df['weight'] > 0][['id1', 'id2', 'weight']].to_csv('hearst_ppmi_filtered.csv', index=False)
+    df[df['weight'] > 0][['id1', 'id2', 'weight']]\
+        .to_csv('hearst_ppmi_filtered.csv', index=False)
 
 if not os.path.exists('data/bless.tsv'):
     data_dir = os.environ.get('HYPERNYMY_DATA_DIR', 'data')
