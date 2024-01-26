fa85462673f85b862a5fca1e04314d55e193b587
generatedunixname89002005287564 <generatedunixname89002005287564@meta.com>
2024-01-26 06:44:59 -0800

    deeplearning/projects/hyperbolic-embeddings
    Reviewed By: zsol
    
    Differential Revision: D53083690
    
    fbshipit-source-id: 08ad9ee027925562e0b0a5b489a97bdd506f7c0e

--- hypernymy_eval.py
+++ hypernymy_eval.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python3
 
-import json
-from hype.hypernymy_eval import main as hype_eval
 import argparse
+import json
+
+from hype.hypernymy_eval import main as hype_eval
 
 
-if __name__ == '__main__':
+def main() -> None:
     parser = argparse.ArgumentParser()
-    parser.add_argument('file')
+    parser.add_argument("file")
     opt = parser.parse_args()
 
     results, summary = hype_eval(opt.file, cpu=False)
 
     print(json.dumps(results))
+
+
+if __name__ == "__main__":
+    main()  # pragma: no cover
